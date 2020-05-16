1874e7277b4615ff108489b0b08cfeaa6cfa922e
fafieshamie <62053799+fafieshamie@users.noreply.github.com>
2020-05-17 03:26:50 +1200

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.fileView, name='post_list'),
     path('post/<int:pk>/',views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
-
+    path('post', views.post1, name='post1'),
+    path('post', views.post2, name='post2'),
+    path('post', views.post3, name='post3'),
     ]
