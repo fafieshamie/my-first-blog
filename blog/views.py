32f45c331ed3b01881d633d910d0270131c38849
fafieshamie <62053799+fafieshamie@users.noreply.github.com>
2020-05-17 03:29:00 +1200

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.utils import timezone
-from.models import Post
+from .models import Post
 from.forms import PostForm
 from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
@@ -9,10 +9,24 @@ from django.shortcuts import render, get_object_or_404
 def fileView(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return	render(request,'blog/post_list.html', {'posts': posts})
+
+def post1(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return	render(request,'blog/post1.html', {'posts': posts})
   
+def post2(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return	render(request,'blog/post2.html', {'posts': posts})
+    
+def post3(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return	render(request,'blog/post3.html', {'posts': posts})
+    
 def post_detail(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return	render(request,'blog/post_detail.html', {'posts': posts})
+    
+
     
 def	post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -50,7 +64,7 @@ def	post_new(request, pk):
 def	post_edit(request,	pk):				
     post = get_object_or_404(Post,	pk=pk)				
     if	request.method == "POST":								
-        form = PostForm(request.POST,	instance=post)
+        form = PostForm(request.POST, instance=post)
         if	form.is_valid():												
             post = form.save(commit=False)												
             post.author = request.user												
@@ -61,4 +75,7 @@ def	post_edit(request,	pk):
         form = PostForm(instance=post)				
         return render(request, 'blog/post_edit.html', {'form':	form})
         
+def post_list(request):
+    post = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'post' : posts})
 
